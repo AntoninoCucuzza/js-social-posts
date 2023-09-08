04e4fb3423b832be3487f61e35c869025acbba10
AntoninoCucuzza <cucuzza.antonino@hotmail.com>
2023-09-08 15:52:36 +0200

    milestone 2

--- assets/js/main.js
+++ assets/js/main.js
@@ -41,10 +41,12 @@ Buon divertimento e buon weekend! Confermate lettura come al solito! :baby-yoda:
 
 console.log('we');
 
+const containerEl = document.getElementById('container')
+
 const posts = [
     {
         "id": 1,
-        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "content": "lorem1 Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
         "media": "https://unsplash.it/600/300?image=171",
         "author": {
             "name": "Phil Mangione",
@@ -56,7 +58,7 @@ const posts = [
     },
     {
         "id": 2,
-        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "content": "lorem2 Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
         "media": "https://unsplash.it/600/400?image=112",
         "author": {
             "name": "Sofia Perlari",
@@ -67,7 +69,7 @@ const posts = [
     },
     {
         "id": 3,
-        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "content": "lorem3 Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
         "media": "https://unsplash.it/600/400?image=234",
         "author": {
             "name": "Chiara Passaro",
@@ -79,8 +81,8 @@ const posts = [
     },
     {
         "id": 4,
-        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
-        /* "media": "https://unsplash.it/600/400?image=24", */
+        "content": "lorem4 Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+         "media": "https://unsplash.it/600/400?image=24", 
         "author": {
             "name": "Luca Formicola",
             "image": null
@@ -91,14 +93,69 @@ const posts = [
     },
     {
         "id": 5,
-        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
-       /*  "media": "https://unsplash.it/600/400?image=534", */
+        "content": "lorem5 Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/400?image=534",
         "author": {
             "name": "Alessandro Sainato",
             "image": "https://unsplash.it/300/300?image=29"
         },
         "likes": 95,
         "created": "03-05-2021"
-
+        
     }
 ];
+
+posts.forEach(key => {
+    containerEl.insertAdjacentHTML('beforeend', postMarkup(key.author.image, key.author.name, key.created, key.content, key.media, key.likes,))
+    
+});
+    
+
+
+
+
+
+
+
+
+
+
+
+
+function postMarkup(avatarImg, Name, data, content,  postImg, likes){
+    const markupPost =`
+    
+    <div class="post">
+    <div class="post__header">
+        <div class="post-meta">                    
+            <div class="post-meta__icon">
+                <img class="profile-pic" src="${(avatarImg === null ? './assets/img/avatar.jpg' : avatarImg)}" alt="Phil Mangione">                    
+            </div>
+            <div class="post-meta__data">
+                <div class="post-meta__author">${Name}</div>
+                <div class="post-meta__time">${data}</div>
+            </div>                    
+        </div>
+    </div>
+    <div class="post__text">${content}</div>
+    <div class="post__image">
+        <img src="${postImg}" alt="">
+    </div>
+    <div class="post__footer">
+        <div class="likes js-likes">
+            <div class="likes__cta">
+                <a class="like-button  js-like-button" href="#" data-postid="1">
+                    <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                    <span class="like-button__label">Mi Piace</span>
+                </a>
+            </div>
+            <div class="likes__counter">
+                Piace a <b id="like-counter-1" class="js-likes-counter">${likes}</b> persone
+            </div>
+        </div> 
+    </div>            
+    </div>
+    `
+
+    return markupPost
+};
